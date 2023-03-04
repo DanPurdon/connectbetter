d0a110aa6cc4cd66340c0ce391a379c4c6c506e8
Dan Purdon <danpurdon@hotmail.com>
2023-03-03 21:34:28 -0600

    category CRUD all pointing to new server, working correctly

--- src/components/managers/CategoryManager.js
+++ src/components/managers/CategoryManager.js
@@ -6,3 +6,33 @@ export const getUserCategories = () => {
     })
         .then(response => response.json())
 }
+
+export const deleteCategory = (categoryId) => {
+    return fetch(`http://localhost:8000/categories/${categoryId}`, {
+        method: "DELETE",
+        headers: {"Authorization": `Token ${localStorage.getItem("connect_token")}`}
+    })
+}
+
+export const addCategory = (categoryObject) => {
+    return fetch("http://localhost:8000/categories", { 
+        method: "POST",
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("connect_token")}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(categoryObject)
+    })
+        .then(response => response.json())
+}
+
+export const editCategory = (categoryObject) => {
+    return fetch(`http://localhost:8000/categories/${categoryObject.id}`, {
+        method: "PUT",
+        headers: {
+            "Authorization": `Token ${localStorage.getItem("connect_token")}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(categoryObject)
+    })
+}
